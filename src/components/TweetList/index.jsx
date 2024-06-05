65fb434de1416ca5b59457d66cb20549b0a22223
Diontae Akins <diontaea@gmail.com>
2024-06-05 11:16:01 -0400

    3

--- src/components/TweetList/index.jsx
+++ src/components/TweetList/index.jsx
@@ -4,40 +4,38 @@ import Tweet from "../Tweet";
 
 import "./TweetList.module.css";
 import CreateTweetForm from "../CreateTweet";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 function TweetList() {
   const [tweets, setTweets] = useState(data);
 
   const addTweet = (newTweet) => {
     const tweetDoc = {
-        content:newTweet,
-        username:'hellome',
-        likes:0,
-        retweets:0,
-        timstamp:new Date(),
-        id: uuidv4()
-    }
-    setTweets([tweetDoc, ...tweets])
-  }
-  const removeTweet = (tweetId) =>{
-    setTweets(tweets.filter(t => t.id !== tweetId))
-  }
+      content: newTweet,
+      username: "hellome",
+      likes: 0,
+      retweets: 0,
+      timstamp: new Date(),
+      id: uuidv4(),
+    };
+    setTweets([tweetDoc, ...tweets]);
+  };
+  const removeTweet = (tweetId) => {
+    setTweets(tweets.filter((t) => t.id !== tweetId));
+  };
 
   return (
     <div>
-
-
-      <h2>Spilt Tea</h2>
-      <CreateTweetForm addTweet={addTweet}/>
-
+      <CreateTweetForm addTweet={addTweet} />
+      
+<h2>Spilt Tea...</h2>
       <section>
         {tweets.map((item) => (
-          <Tweet tweet= {item} key={item.id} removeTweet={removeTweet}/>
+          <Tweet tweet={item} key={item.id} removeTweet={removeTweet} />
         ))}
       </section>
     </div>
   );
 }
 
-export default TweetList
+export default TweetList;
